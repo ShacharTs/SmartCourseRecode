58d3efe17029d1c9c99129b0e294d10a3860e836
ShacharTs <shachar.ts1@gmail.com>
2025-12-09 14:30:04 +0200

    slowly coming back

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.smartcourse"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.smartcourse"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+    implementation(libs.generativeai)
+    debugImplementation(libs.compose.ui.tooling)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.animation:animation")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation)
+
+
+
+    implementation("io.github.jan-tennert.supabase:gotrue-kt:2.4.0")
+    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.4.0")
+    implementation("io.github.jan-tennert.supabase:compose-auth:2.4.0")
+
+
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+
+
+    implementation("io.ktor:ktor-client-okhttp:2.3.7")
+
+
+    implementation("androidx.compose.material:material-icons-extended:1.7.0")
+}
---
+++ app/src/main/java/com/smartcourse/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.smartcourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import com.smartcourse.auth.AuthViewModel
+import com.smartcourse.navigation.RootNavigation
+import com.smartcourse.ui.theme.SmartCourseTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.getValue
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val authViewModel: AuthViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SmartCourseTheme {
+                RootNavigation(authViewModel)
+            }
+        }
+    }
+}
