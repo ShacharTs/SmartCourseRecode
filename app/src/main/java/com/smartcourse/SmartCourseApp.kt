697c835eed328fb42b62a70be394e2d38cbcd48b
ShacharTs <shachar.ts1@gmail.com>
2025-12-09 18:41:11 +0200

    keep patching the app

--- app/src/main/java/com/smartcourse/SmartCourseApp.kt
+++ app/src/main/java/com/smartcourse/SmartCourseApp.kt
@@ -1,7 +1,16 @@
 package com.smartcourse
 
 import android.app.Application
+import com.smartcourse.data.remote.supbase.SupabaseClientProvider
+import com.smartcourse.di.SupabaseModule
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SmartCourseApp : Application()
+class SmartCourseApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SupabaseClientProvider.init(this)
+    }
+}
--- app/src/main/java/com/smartcourse/di/SupabaseModule.kt
+++ app/src/main/java/com/smartcourse/di/SupabaseModule.kt
@@ -1,15 +1,11 @@
 package com.smartcourse.di
 
-import android.content.Context
+import com.smartcourse.data.remote.supbase.SupabaseClientProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.Auth
-import io.github.jan.supabase.postgrest.Postgrest
 import javax.inject.Singleton
 
 @Module
@@ -18,15 +14,8 @@ object SupabaseModule {
 
     @Provides
     @Singleton
-    fun provideSupabaseClient(
-        @ApplicationContext context: Context
-    ): SupabaseClient {
-        return createSupabaseClient(
-            supabaseUrl = "https://ronaetarblsnyuakbxsv.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvbmFldGFyYmxzbnl1YWtieHN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDk4NzIsImV4cCI6MjA3ODYyNTg3Mn0.SvfcUUP2Qmp501WB4iHOpnTi21kt-rS-uMPnzWqJyOk"
-        ) {
-            install(Auth)
-            install(Postgrest)
-        }
+    fun provideSupabaseClient(): SupabaseClient {
+        return SupabaseClientProvider.client
     }
 }
+
